be5c57933ea6d9033b5dd746578eb516d46ed0cb
Your Name <you@example.com>
2024-07-07 14:32:49 +0800

    feat: Build source if AIDL file has changed

--- dist/index.js
+++ dist/index.js
@@ -47,15 +47,15 @@ let ApkBuilder = ApkBuilder_1 = class ApkBuilder {
             if (changes.has(this.config.manifest)) {
                 this.queue.buildManifest();
             }
+            if (!this.lastAIDL && changes.has(this.config.code)) {
+                this.queue.buildSource();
+            }
             if (this.config.aidl && changes.has(this.config.aidl)) {
                 this.queue.buildAidl();
                 this.lastAIDL = true;
             }
             else {
                 this.lastAIDL = false;
-            }
-            if (!this.lastAIDL && changes.has(this.config.code)) {
-                this.queue.buildSource();
             }
             if (changes.has(this.config.assets)) {
                 this.queue.buildAssets();
